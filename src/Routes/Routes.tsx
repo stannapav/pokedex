19350613dafe1914d77daed143a957bc000d7ff9
Anna Striharchuk <stannapav@gmail.com>
2024-09-29 22:34:48 +0300

    adding new path to router

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
+import ListPage from "../Pages/ListPage/ListPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "", element: <HomePage /> }],
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "/list", element: <ListPage /> },
+    ],
   },
 ]);
